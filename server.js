b51c58e9db8236bd305161e5abef768d8c18db8a
abodalnemari444-hue <m7nmri@gmail.com>
2026-08-09 18:45:06 +0300

    Rename restaurant to Ya Habayeb

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ async function sendVerificationSms(toPhone, code) {
   const body = new URLSearchParams({
     To: toPhone,
     From: TWILIO_FROM_NUMBER,
-    Body: `رمز التحقق الخاص بك في مطعم النمري: ${code}`,
+    Body: `رمز التحقق الخاص بك في مطعم يا حبايب: ${code}`,
   });
   const resp = await fetch(`https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json`, {
     method: 'POST',
